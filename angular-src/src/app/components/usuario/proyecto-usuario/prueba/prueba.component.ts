09ff7904aa297e2b8104916fadbc8c5640601aef
tecmelo <tec_melo@hotmail.com>
2017-12-19 15:34:23 -0600

    ppdf posicion financiera

--- angular-src/src/app/components/usuario/proyecto-usuario/prueba/prueba.component.ts
+++ angular-src/src/app/components/usuario/proyecto-usuario/prueba/prueba.component.ts
@@ -58,4 +58,165 @@ export class PruebaComponent implements OnInit {
     return r;
   }
 
+
+
+    PDFposicionComparativa(){
+    var doc= new jsPDF({
+    unit: 'mm',
+    format: [279,215.9]
+
+
+  });
+
+    var columns = [
+    {title: "", dataKey: "cara"},
+    {title: "Año Actual", dataKey: "actual"},
+    {title: "Año Anterior", dataKey: "anterior"},
+    {title: "Aplicación", dataKey: "aplicacion"},
+    {title: "Origen", dataKey: "origen"}];
+
+
+    var rows = [
+    {"cara":"Amenos de un Año"},
+    {"cara":"Caja Bancos"},
+    {"cara":"Cuentas por Cobrar"},
+    {"cara":"IVA Acreditable"},
+    {"cara":"Almacen de Articulo Terminado"},
+    {"cara":"Almacen de Materiales"},
+    {"cara":"A más de un año"},
+    {"cara":"Terrenos"},
+    {"cara":"Edificios e Instalaciones" },
+    {"cara":"Deprecicaicón Acumulada"},
+    {"cara":"Maquinaria y  Equipo" },
+    {"cara":"Depreciación Acumulada"},
+    {"cara":"Muebles y Enseres"},
+    {"cara":"Depresisción Acumulada"},
+    {"cara":"Equipo de Transporte"},
+    {"cara":"Depreciación Acumulada"},
+    {"cara":"De Aplicación Diferida"},
+    {"cara":"Pagos hechos por anticipado"},
+    {"cara":"Gastos por Amortizar"},
+    {"cara":"A menos de un año"},
+    {"cara":"IVA por Enterar"},
+    {"cara":"Impuestos por Pagar"},
+    {"cara":"Poveedores"},
+    {"cara":"PTU por Pagar"},
+    {"cara":"Prestamos Bancarios"},
+    {"cara":"A más de un año"},
+    {"cara":"Prestamos Totales"},
+    {"cara":"Capital Social"},
+    {"cara":"Reserva Legal"},
+    {"cara":"Utilidad Acumulada"},
+    {"cara":"Utilidad del Ejercicio"},
+    {"cara":"Total"},
+
+    ];
+
+    var t = {
+      title:"Total",
+      dataKey:"t"
+    }
+
+
+
+    doc.autoTable(columns, rows, {
+    margin: {top: 40,
+         left:15},
+    tableWidth: 185,
+    headerStyles: {fillColor:0},
+    columnStyles: {
+    cara: {halign:'left',columnWidth:65}
+    },
+    addPageContent: function(data) {
+    doc.setFontSize(15);
+    doc.setFontType("bold");
+    doc.text(107.9, 15, 'Proyecto Empresa XYZ SA de CV', null, null, 'center');
+    doc.setFontSize(13);
+    doc.text(107.9, 23, 'Posición Comparativa', null, null, 'center');
+    doc.line(25, 27, 190, 27);
+    },
+
+
+    drawRow: function (row, data) {
+           // Colspan
+           doc.setFontStyle('bold');
+           doc.setFontSize(10);
+           if (row.index === 0) {
+               doc.rect(data.settings.margin.left, row.y, data.table.width, 7, 'S');
+               doc.autoTableText("Derechos", data.settings.margin.left + data.table.width / 2, row.y + row.height / 2, {
+                   halign: 'center',
+                   valign: 'middle'
+               });
+               data.cursor.y += 7;
+           } else if (row.index === 19) {
+               doc.rect(data.settings.margin.left, row.y, data.table.width, 7, 'S');
+               doc.autoTableText("Obligaciones", data.settings.margin.left + data.table.width / 2, row.y + row.height / 2, {
+                   halign: 'center',
+                   valign: 'middle'
+               });
+               data.cursor.y += 7;
+           }else if (row.index === 27) {
+               doc.rect(data.settings.margin.left, row.y, data.table.width, 7, 'S');
+               doc.autoTableText("Con los Accionistas", data.settings.margin.left + data.table.width / 2, row.y + row.height / 2, {
+                   halign: 'center',
+                   valign: 'middle'
+               });
+               data.cursor.y += 7;
+           }
+
+           if (row.index % 5 === 0) {
+               var posY = row.y + row.height * 6 + data.settings.margin.bottom;
+               if (posY > doc.internal.pageSize.height) {
+                   data.addPage();
+               }
+           }
+       },
+       drawCell: function (cell, data) {
+           // Rowspan
+
+           if (data.column.dataKey === 'id') {
+               if (data.row.index % 5 === 0) {
+                   doc.rect(cell.x, cell.y, data.table.width, cell.height * 5, 'S');
+                   doc.autoTableText(data.row.index / 5 + 1 + '', cell.x + cell.width / 2, cell.y + cell.height * 5 / 2, {
+                       halign: 'center',
+                       valign: 'middle'
+                   });
+               }
+               return false;
+           }
+
+
+           var rows = data.table.rows;
+           if (data.row.index == 0) {
+             doc.setFillColor(176, 176, 176);
+             doc.setFontType("bold")
+           }else if (data.row.index == 6) {
+             doc.setFillColor(176, 176, 176);
+             doc.setFontType("bold")
+           }else if (data.row.index ==16) {
+             doc.setFillColor(176, 176, 176);
+             doc.setFontType("bold")
+           }else if (data.row.index == 19) {
+             doc.setFillColor(176, 176, 176);
+             doc.setFontType("bold");
+           }else if (data.row.index == 25) {
+             doc.setFillColor(176, 176, 176);
+             doc.setFontType("bold");
+           }
+
+       }
+
+
+
+
+
+
+    });
+
+    doc.save("Estado de Resultados.pdf");
+
+
+
+    }
+
 }
