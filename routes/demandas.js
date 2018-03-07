0f09d46581918df4381d48ca8a9de804b5b73250
Alfredo Esquivel <alfred95esquivel@gmail.com>
2018-03-06 23:45:27 -0600

    graficas por niveles

--- routes/demandas.js
+++ routes/demandas.js
@@ -4,11 +4,6 @@ const config = require('../config/db');
 const demanda = require('../models/demanda');
 const Promise = require("bluebird");
 
-const zonaArray = [];
-const repIdZonas = [];//almacena las veces que se repite un idZona en productozonasindes
-const repPeriodos = [];
-const periodosArray = [];
-
 router.post('/register', (req, res, next) => {
   Promise.resolve()
   .then(function () {
@@ -95,7 +90,7 @@ router.post('/delete', (req, res, next) => {
     return demanda.getDemandaZona(idZona,idProducto);
   })
   .then(function(rows){
-    var numPeriodo = rows.length - 1;
+    var numPeriodo = rows.length;
     var idZona = req.body.idZona;
     var idProducto = req.body.idProducto;
 
@@ -140,6 +135,19 @@ router.post('/getdemanda', (req, res, next) => {
 });
 
 router.get('/grafica', (req, res, next) => {
+  //Guardamos las consultas
+  var arrayProductoZonaDemanda = [];
+  var arrayIdZonas = [];
+  var arrayZonas = [];
+  var arrayFilter = [];
+  var arrayIdProductoEnZona = [];
+
+  //Arreglos que generan el json
+  var repIdZonas = [];//almacena las veces que se repite un idZona en productozonasindes
+  var zonaArray = [];
+  var repPeriodos = [];
+  var periodosArray = [];
+
   Promise.join(
     demanda.getProductoZonaDemanda(),
     demanda.getIdZonasFromProductoZonaDemanda(),
@@ -147,30 +155,46 @@ router.get('/grafica', (req, res, next) => {
     demanda.filterIdZonaIdProducto(),
     demanda.idProductoEnZona(),
     function(productozonademanda,idzonas,zonas,filter,idproductoenzona) {
-      return jsonProductoZonaDemanda(productozonademanda,idzonas,zonas,filter,idproductoenzona);
+
+      arrayProductoZonaDemanda = productozonademanda;
+      arrayIdZonas = idzonas;
+      arrayZonas = zonas;
+      arrayFilter = filter;
+      arrayIdProductoEnZona = idproductoenzona;
+
+      repIdZonas = repeticionesIdZona(arrayIdZonas,arrayFilter);
+      repPeriodos = repeticionesPeriodo(arrayFilter,arrayProductoZonaDemanda);
      })
-  .then(function (rows) {
-      res.json({success: true, datos:rows, msg:"Operacion exitosa"});
-      repIdZonas.length=0;
-      repPeriodos.length=0;
-      periodosArray.length=0;
-      zonaArray.length=0;
-  })
-  .catch(function (err) {
-    console.error("got error: " + err);
-    if (err instanceof Error) {
-      res.status(400).send("Error general");
-      console.log(err);
-    } else {
-      res.status(200).json({ "code": 1000, "message": err });
-    }
+     .then(function () {
+       return zonaArray = jsonZona(zonaArray,arrayIdZonas,arrayZonas);
+     })
+    .then(function (zonaArray) {
+      return jsonProducto(zonaArray,repIdZonas,arrayIdProductoEnZona);
+    })
+    .then(function (zonaArray) {
+      return jsonPeriodo(repIdZonas,repPeriodos,zonaArray,arrayProductoZonaDemanda);
+    })
+    .then(function (rows) {
+        res.json({success: true, datos:rows, msg:"Operacion exitosa"});
+        repIdZonas.length=0;
+        repPeriodos.length=0;
+        periodosArray.length=0;
+        zonaArray.length=0;
+    })
+    .catch(function (err) {
+      console.error("got error: " + err);
+      if (err instanceof Error) {
+        res.status(400).send("Error general");
+        console.log(err);
+      } else {
+        res.status(200).json({ "code": 1000, "message": err });
+      }
+    });
   });
-});
 
-function jsonProductoZonaDemanda(productozonademanda,idzonas,zonas,filter,idproductoenzona) {
-
-
-
+function repeticionesIdZona(idzonas,filter) {
+  //Repeticiones idZonas en demanda
+  var arrayRepIdZonas = [];
   var i = 0;
   while (i<idzonas.length) {
     var aux = 0;
@@ -179,148 +203,99 @@ function jsonProductoZonaDemanda(productozonademanda,idzonas,zonas,filter,idprod
         aux = aux +1;
       }
     }
-    repIdZonas.push(aux);
+    arrayRepIdZonas.push(aux);
     i++;
   }
-//console.log("repIdZonas: "+repIdZonas);
-
-
-var k = 0;
-
-while (k < idzonas.length) {
-  for (var i = 0; i < zonas.length; i++) {
-    if (idzonas[k].Zona_idZonas == zonas[i].idZona) {
-      var json = {
-        "idZona":idzonas[k].Zona_idZonas,
-        "nombreZona":zonas[i].nombreZona,
-        "productos":[]
-      }
-    zonaArray.push(json);
-    }
-  }
-  k++;
-}
-//console.log(zonaArray);
-
-// for(var i=0; i<productozonademanda.length; i++){
-//   console.log(productozonademanda[i]);
-// }
-//console.log("idproductoenzona: ",idproductoenzona);
-//productos insercion
-var aux2 = 0;
-for (var j = 0; j < repIdZonas.length; j++) {
-  for (var k = 0; k < (repIdZonas[j]); k++) {
-    var producto = {
-      "idProducto":idproductoenzona[aux2].Producto_idProducto,
-      "color":idproductoenzona[aux2].color,
-      "periodos":[]
-    }
-    zonaArray[j]['productos'].push(producto);
-  //  zonaArray[j]['productos'][k]['periodos'].push('a');
-   aux2 = aux2 + 1;
-  }
+  return arrayRepIdZonas;
 }
 
-
-//creamos periodos
-var m = 0;
-
-
-  while (m < filter.length) {
-    var aux3 = 0;
-    for (var n = 0; n < productozonademanda.length; n++) {
-      if (filter[m].Zona_idZonas == productozonademanda[n].Zona_idZonas &&
-         filter[m].Producto_idProducto == productozonademanda[n].Producto_idProducto) {
-          aux3 = aux3 + 1;
-        // var json = {
-        //   "numPeriodo":productozonademanda[n].numPeriodo,
-        //   "cantidad":productozonademanda[n].cantidad
-        // }
-        // periodosArray.push(json);
+function jsonZona(zonaArray,idzonas,zonas) {
+  //ZONA
+  var k = 0;
+  while (k < idzonas.length) {
+    for (var i = 0; i < zonas.length; i++) {
+      if (idzonas[k].Zona_idZonas == zonas[i].idZona) {
+        var json = {
+          "idZona":idzonas[k].Zona_idZonas,
+          "nombreZona":zonas[i].nombreZona,
+          "productos":[]
+        }
+      zonaArray.push(json);
       }
     }
-    repPeriodos.push(aux3);
-    m++;
+    k++;
   }
+  return zonaArray;
+}
 
-// console.log(repPeriodos);
-//
-//
-// for (var i = 0; i < periodosArray.length; i++) {
-//     console.log(periodosArray[i]);
-// }
-
-var aux5 = 0;//aux5 cuenta hasta 3
-var aux6 = 0;//aux6 cuenta hasta 4
-
-// console.log("repIdZonas: "+repIdZonas);
-//
-// console.log("repPeriodos: ",repPeriodos);
-// console.log("zonaArray: ",zonaArray);
-
-//Cada índice representa la cantidad de productos por zona
-for (var j = 0; j < repIdZonas.length; j++) {//numero de idZona en el json: 2
-  //Iteramos los productos que tiene cada zona
-  for (var k = 0; k < (repIdZonas[j]); k++) {//numero de idProductos dentro de productos: 1,2. El de arriba es su tam
-    //Cada índice guarda la cantidad de productos demandados en todos los periodos financieros
-    //e.i. la cantidad total individual de productos x,y,z,etc. en zonas A,B,C que hay por periodo financiero 1,2,...,n
-    //E.G.
-    /*
-    numPeriodo | Producto_idProducto | Zona_idZona |
-+------------+----------+--------------+------------
-|          1 |                   4 |          84 |
-|          2 |                   4 |          84 |
-|          3 |                   4 |          84 |
-|          4 |                   4 |          84 |
-|          5 |                   4 |          84 |
-|          6 |                   4 |          84 |
-|          7 |                   4 |          84 |
-|          8 |                   4 |          84 |
-
-    */
-    for (var l = 0; l < repPeriodos[aux5]; l++) {//1,2,1. aux5 sirve para barrer el arreglo de repeticiones de periodos
-/*
-hacemos un for con 1,2,1
-que barra:
-  0,
-  0,1
-  0
-con: 1,2,1
-*/
-//zonaArray[j].idZona
-//console.log("zonaArray[j].idZona: ",zonaArray[j].idZona);
-//console.log("zonaArray[j]['productos'][k].idProducto: ",zonaArray[j]['productos'][k].idProducto);
-
-
-//productozonademanda[j].Zona_idZonas
-//console.log("productozonademanda[l].Zona_idZonas: ",productozonademanda[aux6].Zona_idZonas);
-//console.log("productozonademanda[l].Producto_idProducto: ",productozonademanda[aux6].Producto_idProducto);
-
-//Validación que asegura que lo que ya está en el arreglo es exactamente lo mismo que lo que está
-//en el query productozonademanda. Asegurando que los datos correspondan a los valores que se desean
-
- if (zonaArray[j].idZona == productozonademanda[aux6].Zona_idZonas &&
- zonaArray[j]['productos'][k].idProducto == productozonademanda[aux6].Producto_idProducto) {
-
-   var json = {
-     "numPeriodo":productozonademanda[aux6].numPeriodo,
-     "cantidad":productozonademanda[aux6].cantidad
-   }
-
-//zonaArray[j]['productos'][k]['periodos'].push(periodosArray[aux6]);
-zonaArray[j]['productos'][k]['periodos'].push(json);
-
- }
-
-      aux6=aux6+1;
+function jsonProducto(zonaArray,repIdZonas,idproductoenzona) {
+  //PRODUCTOS
+  var aux2 = 0;
+  for (var j = 0; j < repIdZonas.length; j++) {
+    for (var k = 0; k < (repIdZonas[j]); k++) {
+      var producto = {
+        "idProducto":idproductoenzona[aux2].Producto_idProducto,
+        "color":idproductoenzona[aux2].color,
+        "periodos":[]
+      }
+      zonaArray[j]['productos'].push(producto);
+    //  zonaArray[j]['productos'][k]['periodos'].push('a');
+     aux2 = aux2 + 1;
     }
+  }
+  return zonaArray;
+}
+
+function repeticionesPeriodo(filter,productozonademanda) {
+  //Repeticiones Periodo
+  var arrayRepPeriodos = [];
+  var m = 0;
+    while (m < filter.length) {
+      var aux3 = 0;
+      for (var n = 0; n < productozonademanda.length; n++) {
+        if (filter[m].Zona_idZonas == productozonademanda[n].Zona_idZonas &&
+           filter[m].Producto_idProducto == productozonademanda[n].Producto_idProducto) {
+            aux3 = aux3 + 1;
+          // var json = {
+          //   "numPeriodo":productozonademanda[n].numPeriodo,
+          //   "cantidad":productozonademanda[n].cantidad
+          // }
+          // periodosArray.push(json);
+        }
+      }
+      arrayRepPeriodos.push(aux3);
+      m++;
+    }
+    return arrayRepPeriodos;
+}
+
+function jsonPeriodo(repIdZonas,repPeriodos,zonaArray,productozonademanda) {
+  //PERIODO
+  var aux5 = 0;//aux5 cuenta hasta 3
+  var aux6 = 0;//aux6 cuenta hasta 4
+
+  //Cada índice representa la cantidad de productos por zona
+  for (var j = 0; j < repIdZonas.length; j++) {//numero de idZona en el json: 2
+    //Iteramos los productos que tiene cada zona
+    for (var k = 0; k < (repIdZonas[j]); k++) {//numero de idProductos dentro de productos: 1,2. El de arriba es su tam
+      //Cada índice guarda la cantidad de productos demandados en todos los periodos financieros
+      //e.i. la cantidad total individual de productos x,y,z,etc. en zonas A,B,C que hay por periodo financiero 1,2,...,n
+      for (var l = 0; l < repPeriodos[aux5]; l++) {//1,2,1. aux5 sirve para barrer el arreglo de repeticiones de periodos
+        if (zonaArray[j].idZona == productozonademanda[aux6].Zona_idZonas &&
+            zonaArray[j]['productos'][k].idProducto == productozonademanda[aux6].Producto_idProducto) {
+             var json = {
+               "numPeriodo":productozonademanda[aux6].numPeriodo,
+               "cantidad":productozonademanda[aux6].cantidad
+             }
+            //zonaArray[j]['productos'][k]['periodos'].push(periodosArray[aux6]);
+            zonaArray[j]['productos'][k]['periodos'].push(json);
+          }
+        aux6=aux6+1;
+      }
       aux5 = aux5 + 1;
+    }
   }
+  return zonaArray;
 }
-
-return zonaArray;
-}
-
-
 
 module.exports = router;
